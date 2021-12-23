64a42a135bb15475c52ebe443551ba6cfca59159
Hasnain <hasnainmehmood3435@gmail.com>
2021-12-23 19:40:30 +0300

    try-except

--- app.py
+++ app.py
@@ -114,21 +114,23 @@ if user_menu == 'Country-wise analysis':
     country_list = df['region'].dropna().unique().tolist()
     country_list.sort()
     selected_country = st.sidebar.selectbox('Select a country', country_list)
+    try:
+        country_df = helper.yearwise_medal_tally(df, selected_country)
+        fig = px.line(country_df , x= 'Year', y = 'Medal')
+        st.title(selected_country + ' Medal Tally over the years')
+        st.plotly_chart(fig)
 
-    country_df = helper.yearwise_medal_tally(df, selected_country)
-    fig = px.line(country_df , x= 'Year', y = 'Medal')
-    st.title(selected_country + ' Medal Tally over the years')
-    st.plotly_chart(fig)
+        st.title(selected_country + ' excels in the following Sports')
+        pt = helper.country_event_heatmap(df, selected_country)
+        fig , ax = plt.subplots(figsize=(20,20))
+        ax = sns.heatmap(pt, annot= True)
+        st.pyplot(fig)
 
-    st.title(selected_country + ' excels in the following Sports')
-    pt = helper.country_event_heatmap(df, selected_country)
-    fig , ax = plt.subplots(figsize=(20,20))
-    ax = sns.heatmap(pt, annot= True)
-    st.pyplot(fig)
-
-    st.title( "Top 10 athletes of "+ selected_country)
-    top10_df = helper.most_successful_countrywise(df, selected_country)
-    st.table(top10_df)
+        st.title( "Top 10 athletes of "+ selected_country)
+        top10_df = helper.most_successful_countrywise(df, selected_country)
+        st.table(top10_df)
+    except:
+        st.header('Please select a different country as this is not applicable to the selected country')
 if user_menu == 'Athlete-wise Analysis':
     athlete_df = df.drop_duplicates(subset=['Name', 'region'])
     x1 = athlete_df['Age'].dropna()
